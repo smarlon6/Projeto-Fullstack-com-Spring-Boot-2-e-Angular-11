3e39f6bbb3a1ee0b2911a8f7be10622f64ae02c0
smarlon6 <75749309+smarlon6@users.noreply.github.com>
2021-03-11 16:40:41 -0300

    primeiro commit criando projeto todo springboot e angular

---
+++ todo/src/main/java/br/gov/mt/cuiaba/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mt.cuiaba.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
---
+++ todo/src/test/java/br/gov/mt/cuiaba/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mt.cuiaba.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
